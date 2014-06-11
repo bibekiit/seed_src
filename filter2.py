b4c97083c82a78441b650abeb9a6cca1a7b265c5
bibekiit <bibek.iitkgp@gmail.com>
2014-06-12 01:39:38 +0530

    Update filter2.py

--- filter2.py
+++ filter2.py
@@ -1,5 +1,12 @@
 import scipy.ndimage.filters as snf
 import numpy as np
+
+#############################################################################
+# Author - Bibek Behera                                                     #
+# Date - Feb, 2014                                                          #
+# Place - IIT Bombay                                                        #
+#############################################################################
+
 def filter2(x, b):
     [mx,nx] = x.shape
     stencil = np.rot90(b,2)
